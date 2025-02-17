050e695c66dfb2c246e8a4ac5b9e7a3c597f11b7
Somesh Raj <someshraj78569@gmail.com>
2025-02-17 21:03:36 +0530

    Update FactorialOFN.py

--- Recursion/FactorialOFN.py
+++ Recursion/FactorialOFN.py
@@ -1,20 +1,20 @@
 '''Function Recursion'''
 
-# def Factorial(n: int)->int:
-#     if n == 0:
-#         return 1
-#     return n * Factorial(n - 1)
-# if __name__ == "__main__":
-#     n = int(input())
-#     print(Factorial(n))
+def Factorial(n: int)->int:
+    if n == 0:
+        return 1
+    return n * Factorial(n - 1)
+if __name__ == "__main__":
+    n = int(input())
+    print(Factorial(n))
 
 
 '''Parameterized Recursion'''
-# def Factorial(i, F):
-#     if i < 1:
-#         print(F)
-#         return
-#     Factorial(i - 1, F*i)
-# if __name__ == "__main__":
-#     n = int(input())
-#     Factorial(n, 1)
+def Factorial(i, F):
+    if i < 1:
+        print(F)
+        return
+    Factorial(i - 1, F*i)
+if __name__ == "__main__":
+    n = int(input())
+    Factorial(n, 1)
