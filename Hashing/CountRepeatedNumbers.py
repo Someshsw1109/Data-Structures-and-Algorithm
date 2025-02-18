62a246b8cf7903febd77a0903aae0be0b5b741e7
Somesh Raj <someshraj78569@gmail.com>
2025-02-18 20:22:42 +0530

    Update CountRepeatedNumbers.py

--- Hashing/CountRepeatedNumbers.py
+++ Hashing/CountRepeatedNumbers.py
@@ -15,3 +15,22 @@ arr = list(map(int, input().split()))
 n = int(input())
 res = solution.CountRepeated(arr, n)
 print(res)
+
+
+# Using Hashing
+
+class Somesh:
+    def CountRepeated1(self, arr: List[int], q: int) -> int:
+        n = max(arr) + 1
+        HashCounts = [0] * n
+        for i in range(n):
+            HashCounts[arr[i]] += 1
+        while q:
+            number = int(input())
+            print(HashCounts[number])
+            q -= 1
+somesh = Somesh()
+arr = list(map(int, input().split()))
+q = int(input())
+res = somesh.CountRepeated1(arr, q)
+print(res)
