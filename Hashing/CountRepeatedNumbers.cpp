b7ebaa8e5c457219cb169ee43c94bba13ac0a83b
Somesh Raj <someshraj78569@gmail.com>
2025-02-26 08:44:21 +0530

    Create CountRepeatedNumbers.cpp

---
+++ Hashing/CountRepeatedNumbers.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    //Pre---compute
+    map<int, int> mp; // we take long, long also but it depends on the size of array
+    for (int i = 0; i < n; i++) {
+        mp[arr[i]]++;
+    }
+
+    int q;
+    cin >> q;
+    while (q--) {
+        int number;
+        cin >> number;
+        // Fetch
+        cout << mp[number] << endl;
+    }
+
+    return 0;
+}
