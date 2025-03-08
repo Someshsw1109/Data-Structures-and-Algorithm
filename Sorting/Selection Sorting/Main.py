1af3492df8e7c1ca3fe79469854251c21542a3ca
Somesh Raj <someshraj78569@gmail.com>
2025-03-08 21:32:24 +0530

    Add files via upload

---
+++ Sorting/Selection Sorting/Main.py
@@ -0,0 +1,21 @@
+'''
+Selectionn Sorting --> Pick the minimum element from the list and place it to the first position.
+
+'''
+
+class Solution:
+    def SelectionSort(self, arr, n):
+        for i in range(0, n - 2):
+            Mini = i
+            for j in range(i, n - 1):
+                if arr[j] < arr[Mini]:
+                    Mini = j
+            Temp = arr[Mini]
+            arr[Mini] = arr[i]
+            arr[i] = Temp
+solution = Solution()
+n = int(input())
+arr = list(map(int, input().split()))
+res = solution.SelectionSort(arr, n)
+# print(res)
+print(arr)
